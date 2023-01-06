fdf8b0d08b392190e58b9f3de97c77fe6804b447
Fabri Muller <74914972+FabricioMuller07@users.noreply.github.com>
2023-01-06 15:36:56 -0300

    Add files via upload

---
+++ 2015/Day 1 B.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+	freopen("input.txt","r",stdin);
+	ios_base::sync_with_stdio(false);cin.tie(nullptr);
+	string str; int c=0, p;
+	getline(cin,str);
+	for(size_t i=0;i<str.size();++i){
+		if(str[i]=='(') c++;
+		else c--;
+		if(c==-1) {p=i+1;break;}
+	}
+	cout << p << endl;
+
+	return 0;
+}
+
+
---
+++ 2015/Day 2 A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+//	freopen("input.txt","r",stdin);
+	ios_base::sync_with_stdio(false);cin.tie(nullptr);
+	ll l, w, h, s=0, a; char trash;
+	while(cin >> l >> trash >> w >> trash >>  h){
+		s+=l*w*2 + h*2*w + 2*l*h;
+		a=min(l,min(w,h));
+		if(a==l)s+=a*min(w,h);
+		else if(a==w)s+=a*min(l,h);
+		else if(a==h)s+=a*min(w,l);
+	}
+	cout << s << endl;
+	return 0;
+}
