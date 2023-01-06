089708e6f289505923f7304462f3046b60c0a35a
Fabri Muller <74914972+FabricioMuller07@users.noreply.github.com>
2023-01-06 15:41:33 -0300

    Add files via upload

---
+++ General/Generador de Subconjuntos.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int n, c=0;
+list<list<int>> subconjuntos;
+list<int> l;
+void generador_recursivo(vector<int> &v, int index){
+	c++;
+	subconjuntos.push_back(l);
+	for(int i=index;i<n;++i) { 
+		l.push_back(v[i]);
+		generador_recursivo(v,i+1);
+		l.pop_back();
+	}
+}
+
+
+int main() {
+	cout << "Ingrese cantidad de elementos del conjunto" << endl;
+	cin >> n;
+	cout << "Ingrese elementos del conjunto" << endl;
+	vector<int> v(n);
+	for(int i=0;i<n;++i) { 
+		cin >> v[i];
+	}
+	generador_recursivo(v,0);
+	cout << "cantidad de subconjuntos: " << c << endl;
+	cout << "subconjuntos:" << endl;
+	for(auto it=subconjuntos.begin(); it!=subconjuntos.end(); ++it ) { 
+		for(auto itt=it->begin(); itt!=it->end(); ++itt ) { 
+			cout << *itt << " ";
+		}cout << endl;
+	}
+	return 0;
+}
